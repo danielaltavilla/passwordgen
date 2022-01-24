6cb813795ef96f39aac4111b01fe6aec543c9dbc
Fiora Laurent <groudanex@gmail.com>
2022-01-24 01:31:14 -0500

    window confirm character select

--- develop/script.js
+++ develop/script.js
@@ -1,6 +1,46 @@
 // Assignment code here
 function generatePassword() {
-    console.log("Button working as intended.")
+    console.log("Button working as intended.") //testing purposes only
+
+// step 1. Prompt user for password gen
+
+var promptLength = window.prompt("How long is the password? Please select between 8-128 characters.");
+
+if (promptLength <8 || promptLength >128) {
+    window.alert("You need to provide a length between 8-128 characters!");
+    return generatePassword();
+}
+
+  else { 
+      console.log(promptLength);
+  }
+
+  var chars = [
+      "uppercase letters",
+
+      "lowercase letters",
+
+      "numbers",
+
+      "special chars",
+  ]
+   
+  for (let i = 0; i < 4; i++) {
+      var charsConfirm = window.confirm("Will this password include " + chars[i] + "?");
+
+      if (charsConfirm) {
+
+      }
+  }
+//      1b. Confirm whether upper,lower,numeric,and/or special char
+
+// step 2. Validate the input 
+// step 3. generate password based on criteria 
+
+
+
+// step 4. Display the generated password on page/in an alert.
+    return "generated password";
 }
 
 
